6b10ee27a97da69246d900fcd0ad6e3264a0f3f3
Jacek Kucharczyk <kucjac@gmail.com>
2020-02-14 17:05:38 +0100

    Working with docker

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -4,7 +4,7 @@ curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips_v4
 #curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips_v6
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
-for cfip in `cat /tmp/cf_ips_v4`; do ufw insert 1 allow proto tcp from $cfip comment 'Cloudflare IP'; done
+for cfip in `cat /tmp/cf_ips_v4`; do ufw route allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
 #for cfip in `cat /tmp/cf_ips_v6`; do ufw insert  allow proto tcp from $cfip comment 'Cloudflare IP'; done
 
 ufw reload > /dev/null
